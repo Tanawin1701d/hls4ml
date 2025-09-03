9b55093d333f8283d19960309456ee6e7d7d286c
tanawin <tanawin1701d@outlook.com>
2025-09-04 00:43:56 +0700

    delete unused file and add comment for magic streamer manager

--- hls4ml/backends/vitis_unified_partial/magic_streamer_mng.py
+++ hls4ml/backends/vitis_unified_partial/magic_streamer_mng.py
@@ -1,19 +1,23 @@
 class MgsConMeta:
-    ####
+    # it describes the connection between kernel io and idx of magic streamer
     def __init__(self, io_idx, tensor):
-        self.io_idx = io_idx
-        self.mgs_idx = -1
-        self.mgs_wrap_width = 32
-        self.mgs_row_idx_width = 10
+        self.io_idx = io_idx  # the io Idx of the kernel
+        self.mgs_idx = -1  # magic streamer idx    -1 means DMA
+        self.mgs_wrap_width = 32  # connection width
+        self.mgs_row_idx_width = 10  # magic streamer idx size
+        # it is not specify that it is input or output
 
 
 class MagicBufferMeta:
+    # it specifies the specification of the magic streamer
     def __init__(self, data_width, row_idx_width, mgs_idx):
         self.data_width = data_width
         self.row_idx_width = row_idx_width
         self.mgs_idx = mgs_idx
 
-    def upgrade_mgs_to_support(self, mgs_con_meta):
+    def upgrade_mgs_to_support(self, mgs_con_meta: MgsConMeta):
+        # it will upgrade the buffer size of the magic streamer incase it has been reused
+        # and the system
         if self.row_idx_width < mgs_con_meta.mgs_row_idx_width:
             self.row_idx_width = mgs_con_meta.mgs_row_idx_width
 
@@ -24,22 +28,39 @@ class MagicBufferMeta:
 
 
 class MgsConGraph:
-    # in suppose to be pool of connection for each sub graph
+    # in suppose to be pool of connection for each sub-graph
 
     def __init__(self, gid, input_node_links, amt_graph, mgs_model):  # gid = graph id
-        self.gid = gid
-        self.input_cons = []  # the index of the list supposed to be the index of the input port as well
-        self.output_cons = []
-        self.input_node_links = input_node_links
+        self.gid = gid  # graph id
+        self.input_cons = []  # input port to magic streamer
+        self.output_cons = []  # output port to magic streamer
+        # the index of the input_cons and output_cons supposed to be the index of the kernel's io port as well
+        self.input_node_links = input_node_links  # the link meta data
         self.amt_graph = amt_graph
-        self.mgs_model = mgs_model
+        self.mgs_model = mgs_model  # the main magic streamer pool
 
     def start_convert_graph(self, graph):
-        # do the output first because we need to not free the src first (src des cannot be the same buffer)
+
+        # DO NOT SWAP INPUT LOOP BEFORE OUTPUT LOOP
+        # DO NOT SWAP INPUT LOOP BEFORE OUTPUT LOOP
+        # DO NOT SWAP INPUT LOOP BEFORE OUTPUT LOOP
+        # DO NOT SWAP INPUT LOOP BEFORE OUTPUT LOOP
+
+        # THIS IS VERY CRITICAL
+        # because current magic streamer don't support input and output at the same time
+        # if we do add input connection first the MgsModel class will notice some magic streamer is free
+        # and it will assign to the new output at the same time
+        # we must ensure that magic streamer to store the output at the magic streamer that did not use as the input
+
         out_var = graph.get_output_variables()
         for out_idx, out in enumerate(out_var):
             mgs_con_meta = MgsConMeta(out_idx, out)
             self.add_output_con(mgs_con_meta)
+
+        # DO NOT SWAP INPUT LOOP BEFORE OUTPUT LOOP
+        # DO NOT SWAP INPUT LOOP BEFORE OUTPUT LOOP
+        # DO NOT SWAP INPUT LOOP BEFORE OUTPUT LOOP
+        # DO NOT SWAP INPUT LOOP BEFORE OUTPUT LOOP
 
         # do the input
         in_var = graph.get_input_variables()
@@ -53,13 +74,16 @@ class MgsConGraph:
     def add_input_con(self, mgs_con_meta):
 
         src_gid, src_out_idx = self.input_node_links[self.gid][mgs_con_meta.io_idx]
+        # src graph idx
+        # src_out_idx is the output Idx of the KERNEL not magic streamer idx
 
-        # it load from dma
         if src_gid == -1:
             pass  # src_mgs_idx = -1  # it means dma
         else:
+            # get the magic streamer idx corresponding that contain the input for this port
             src_mgs_con_meta = self.mgs_model.get_mgs_idx(src_gid, src_out_idx)
             mgs_con_meta.mgs_idx = src_mgs_con_meta
+            # tell the master that magic streamer is free because it use the input already
             self.mgs_model.move_buffer_to_free_list(src_mgs_con_meta)
 
         self.input_cons.append(mgs_con_meta)
@@ -70,7 +94,7 @@ class MgsConGraph:
             self.output_cons.append(mgs_con_meta)
             return
 
-        # we check the
+        # we check the available magic stream at the time which current reconfigurable module is operating
         stream_buffer_idx = self.mgs_model.get_existing_possible_mgs_buffer(mgs_con_meta)
 
         if stream_buffer_idx is None:
@@ -79,6 +103,7 @@ class MgsConGraph:
         # upgrade the magic stream buffer to match size of it is lower
         self.mgs_model.upgrade_mgs_to_support(mgs_con_meta, stream_buffer_idx)
 
+        # tel the master  that magicstreamer is occupied
         self.mgs_model.move_buffer_to_using_list(stream_buffer_idx)
         mgs_con_meta.mgs_idx = stream_buffer_idx
 
@@ -150,7 +175,7 @@ class MgsModel:
             )
         return self.con_graphs[gid].input_cons[inputIdx].mgs_idx
 
-    # for outsider usd
+    # for outsider use
     def get_mgs_idx_dst(self, gid, outputIdx):
         if gid not in range(0, self.multigraph.amt_graph):
             raise Exception(
@@ -170,6 +195,7 @@ class MgsModel:
 
     # used by the vitis uniifed partial backend writer and MgsConGraph
     def get_mgs_idx(self, gid, outputIdx):
+        # find the magic streamer index at subgraph gid at output port outputIdx of kernel
         if gid < 0:
             return -1
         mgs_con_meta = self.con_graphs[gid].output_cons[outputIdx]
