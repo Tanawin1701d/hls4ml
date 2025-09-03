dde3becca75260caa5830ed59d9af8ab5a9d72d1
tanawin <tanawin1701d@outlook.com>
2025-09-03 23:33:06 +0700

    fix wrap_gen.py conflict include and driver_gen.py changing template source

--- hls4ml/writer/vitis_unified_partial_writer/driver_gen.py
+++ hls4ml/writer/vitis_unified_partial_writer/driver_gen.py
@@ -9,7 +9,7 @@ class VitisUnifiedPartial_DriverGen(VitisUnified_DriverGen):
     @classmethod
     def write_driver(self, meta: VitisUnifiedWriterMeta, model, mg):
         filedir = os.path.dirname(os.path.abspath(__file__))
-        fin = open(os.path.join(filedir, '../../templates/vitis_unified_partial/driver/pynq/pynq_driver.py'))
+        fin = open(os.path.join(filedir, '../../templates/vitis_unified_partial/driver/pynq/pynq_driver.py.hls4ml'))
         fout = open(f'{model.config.get_output_dir()}/export/pynq_driver.py', 'w')
 
         for line in fin.readlines():
